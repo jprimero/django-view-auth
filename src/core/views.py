69208cf7e00e4f02630ce94ee5230b9e3f3d31f3
Jess Primero <jprimero.ayfvv@slmail.me>
2022-06-29 11:15:20 +0800

    Add the 'user_info' view

--- src/core/views.py
+++ src/core/views.py
@@ -32,3 +32,17 @@ def see_request(request):
     """
 
     return HttpResponse(text, content_type="text/plain")
+
+
+def user_info(request):
+    text = f"""
+        Selected HttpRequest.user attributes:
+
+        username:     {request.user.username}
+        is_anonymous: {request.user.is_anonymous}
+        is_staff:     {request.user.is_staff}
+        is_superuser: {request.user.is_superuser}
+        is_active:    {request.user.is_active}
+    """
+
+    return HttpResponse(text, content_type="text/plain")
