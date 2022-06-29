02e5a2221611f101f9134ba85e6ba6510a73f306
Jess Primero <jprimero.ayfvv@slmail.me>
2022-06-29 11:04:13 +0800

    Add view for inspecting the request

--- src/core/views.py
+++ src/core/views.py
@@ -18,3 +18,17 @@ def view_blog(request, blog_id):
     }
 
     return render(request, "view_blog.html", data)
+
+
+def see_request(request):
+    text = f"""
+        Some attributes of the HttpRequest object:
+
+        scheme: {request.scheme}
+        path:   {request.path}
+        method: {request.method}
+        GET:    {request.GET}
+        user:   {request.user}
+    """
+
+    return HttpResponse(text, content_type="text/plain")
